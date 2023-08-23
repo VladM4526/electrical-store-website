790597a0cdc7361207b521cd8944085edbf6c375
VladM <vdstories20@gmail.com>
2023-08-24 00:04:52 +0200

    added files in repository

---
+++ src/scripts/mobile-menu.js
@@ -0,0 +1,10 @@
+const ssOpenModalBtn = document.querySelector(".el-store-header-mobile-menu-button");
+const ssModal = document.querySelector(".el-store-container");
+const ssCloseModalBtn = document.querySelector(".el-store-mobile-menu-modal-close-button");
+
+ssOpenModalBtn.addEventListener("click", SsToggleModal);
+ssCloseModalBtn.addEventListener("click", SsToggleModal);
+
+function SsToggleModal() {
+    ssModal.classList.toggle("hidden");
+}
